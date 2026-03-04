0875d36cfc5abcf82961b901da8044ae14e61d5c
Daniel Resino <thedan296@gmail.com>
2026-03-04 12:28:38 +0100

    feat: implement camera rig for smooth parallax effect in 3D scene

--- components/canvas/Scene.tsx
+++ components/canvas/Scene.tsx
@@ -1,7 +1,30 @@
 'use client';   // This component is a client component (necessary on 3D)
 
-import { Canvas } from "@react-three/fiber";    // The principal component of the 3D world
+import { Canvas, useFrame } from "@react-three/fiber";    // The principal component of the 3D world
 import { Stars, Float } from "@react-three/drei";    // Component to add stars and float effect
+import { useRef } from 'react'; // Component that take a reference in the page
+import * as THREE from 'three';
+import { useMouse } from '@/hooks/useMouse';
+
+function Rig() {
+
+    const { x, y } = useMouse();
+    const group = useRef<THREE.Group>(null!);
+
+    // useFrame execute 60 times per second
+
+    useFrame((state) => {
+
+        // Move camera slowly to mouse position
+
+        state.camera.position.lerp(new THREE.Vector3(x * 2, y * 2, 5), 0.05);
+        state.camera.lookAt(0, 0, 0)
+
+    });
+
+    return <group ref={group} />;
+
+}
 
 export default function Scene() {
     return (
@@ -12,18 +35,20 @@ export default function Scene() {
             camera={{ position: [0, 0, 5], fov: 75 }} // Camera configuration (position Z and field of view)
             style={{
                 position: 'fixed',  // Fixed position
-                top: 0,
-                left: 0,
+                inset: 0,
                 pointerEvents: 'none' // It allow to click on the bottons of the page
             }}
         >
             {/* ambientLight: Light that illuminates all scene to don't see black  */}
             <ambientLight intensity={0.5} />
+
+            <Rig />
+
             {/* stars: Add stars to the scene */}
             <Stars
                 radius={100}  // Radius of the stars
                 depth={50}    // Depth of the stars
-                count={5000}  // Number of stars
+                count={7000}  // Number of stars
                 factor={4}    // Size of the stars
                 saturation={0} // Color (0 = white)
                 fade            // Make stars fade at the distance
